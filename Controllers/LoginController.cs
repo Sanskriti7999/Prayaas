ac201581d5f2de77a786fcc569c8eb46a055dd00
Sanskriti7999 <106594496+Sanskriti7999@users.noreply.github.com>
2022-07-12 18:39:23 +0530

    Add files via upload

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Prayaas_Website.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prayaas_Website.Controllers
+{
+	public class LoginController : Controller
+	{
+		private IConfiguration _config;
+		public LoginController(IConfiguration config)
+		{
+			_config = config;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[AllowAnonymous]
+		[HttpPost]
+		public IActionResult Login([FromBody] User login)
+		{
+			IActionResult response = Unauthorized();
+			var user = AuthenticateUser(login);
+
+			if (user != null)
+			{
+				var tokenString = GenerateJSONWebToken(user);
+				response = Ok(new { token = tokenString });
+			}
+
+			return response;
+		}
+
+		private string GenerateJSONWebToken(User userInfo)
+		{
+			var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+			var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+			var claims = new[] {
+		new Claim(JwtRegisteredClaimNames.Sub, userInfo.UserName),
+		new Claim(JwtRegisteredClaimNames.Email, userInfo.EmailAddress),
+		new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+	};
+
+			var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+			  _config["Jwt:Issuer"],
+			  null,
+			  expires: DateTime.Now.AddMinutes(120),
+			  signingCredentials: credentials);
+
+			return new JwtSecurityTokenHandler().WriteToken(token);
+		}
+
+		private User AuthenticateUser(User login)
+		{
+			User user = null;
+
+			
+			if (login.UserName == userInfo.UserName)
+			{
+				user = new User { UserName = userInfo.UserName, EmailAddress = userInfo.EmailAddress };
+			}
+			return user;
+		}
+	}
+
+}
+
